81767fefd0ad29ac32532f1c144769694a0b4b50
amirhdre <daraieamirh@gmail.com>
2021-04-17 21:25:10 +0430

    Add HW3_9733023_Code_Q01.py

---
+++ HW3/HW3_9733023_Code_Q01.py
@@ -0,0 +1,28 @@
+'''
+___OVERVIEW___
+CODE FOR QUESTION 1 - HW 3
+
+___SUMMARY___
+Smoothing and Sharpening
+
+___AUTHOR___
+Amirhossein Daraie — 9733023
+
+___DATE___
+17/April/2021
+28/01/1400
+
+'''
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+m = np.array([[0,0,10,10,0],
+              [0,10,20,20,10],
+              [0,10,20,20,10],
+              [0,0,10,10,0],
+              [0,0,0,0,0]])
+
+
+
+
